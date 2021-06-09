951942a99dcea4e450c2c38e2a8761870ae26314
Viktor Prykhidko <viktor.prykhidko@softwarium.net>
2021-06-09 16:38:00 +0300

    Configure azurite and example of work with blob

--- Jupiter.Api/Controllers/WeatherForecastController.cs
+++ Jupiter.Api/Controllers/WeatherForecastController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.IO;
+using Azure.Storage.Blobs;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
 namespace Jupiter.Api.Controllers
@@ -9,18 +12,38 @@ namespace Jupiter.Api.Controllers
     {
         private readonly ILogger<WeatherForecastController> _logger;
         private readonly ApplicationDbContext _context;
-        public WeatherForecastController(ILogger<WeatherForecastController> logger, ApplicationDbContext context)
+        private readonly IConfiguration _configuration;
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, ApplicationDbContext context, IConfiguration configuration)
         {
             _logger = logger;
             _context = context;
+            this._configuration = configuration;
         }
 
-        [HttpGet]
-        public IActionResult Get()
+        [HttpGet("/database")]
+        public IActionResult GetFromDb()
         {
             var entities = _context.Entities;
 
             return Ok(entities);
+        }
+
+        [HttpGet("/azure-storage")]
+        public IActionResult GetFromAzureStorage()
+        {
+            var connectionString = _configuration["Dependencies:Azure:Blob"];
+            var container = "container";
+            var blob = "blob";
+
+            var blobServiceClient = new BlobServiceClient(connectionString);
+            blobServiceClient.DeleteBlobContainer(container);
+            blobServiceClient.CreateBlobContainer(container);
+            var containerClient = blobServiceClient.GetBlobContainerClient(container);
+            var blobClient = containerClient.GetBlobClient(blob);
+            using var stream = new MemoryStream();
+            blobClient.Upload(stream, true);
+            var blobs = containerClient.GetBlobsAsync();
+            return Ok(blobs);
         }
     }
 }
